43ab88d4eff3dd59898e1634431a2072d632478b
liu-liuz <1214katychen@gmail.com>
2021-08-01 03:00:51 -0600

    Basic user database + login function + switch button
    1. Switch button between patient and doctor (√) - change icon and text view when checked.
    2. User database (√) - add one user to test the functionality
    3. Register function (x)
    4. Login function (√)
    5. Basic UI (√) - change text color and button color when pressed, color of the cursor.
    6. Link firebase (√)

--- MedicalClinicApp/app/src/main/java/com/example/medicalclinicapp/MainActivity.java
+++ MedicalClinicApp/app/src/main/java/com/example/medicalclinicapp/MainActivity.java
@@ -2,13 +2,82 @@ package com.example.medicalclinicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+//        User user = new User("user1","12345","Doctor");
+//        ref.child("users").child("u1").setValue(user);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        RadioButton patientButton = (RadioButton) findViewById(R.id.Patient);
+        ImageView img = (ImageView)findViewById(R.id.patient_doctor);
+
+        if (patientButton.isChecked()) {
+            TextView text = (TextView) findViewById(R.id.text_type);
+            text.setText("Patient");
+            img.setImageResource(R.drawable.patient_login);
+        } else {
+            TextView text1 = (TextView) findViewById(R.id.text_type);
+            text1.setText("Doctor");
+            img.setImageResource(R.drawable.doctor_login);
+        }
+    }
+
+    public void login(View view){
+        Button login = findViewById(R.id.login);
+        login.setBackground(getResources().getDrawable(R.drawable.clicked_roundbutton));
+        // Read from the database
+        String username = ((EditText)findViewById(R.id.username)).getText().toString();
+        String password = ((EditText)findViewById(R.id.password)).getText().toString();
+        String type = ((TextView)findViewById(R.id.text_type)).getText().toString();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("users");
+        ValueEventListener listener = new ValueEventListener(){
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot child:dataSnapshot.getChildren()){
+                    User user = child.getValue(User.class);
+                    if(user.getUsername().toString().equals(username) && user.getPassword().toString().equals(password)
+                    && user.getType().equals(type)){
+                        //login succeed
+                        Log.i("info","succeed");
+                    }
+                }
+                //login fail
+                finish();
+                startActivity(getIntent());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Failed to read value
+                Log.w("info", "Failed to read value.", databaseError.toException());
+            }
+        };
+        ref.addValueEventListener(listener);
     }
 }
